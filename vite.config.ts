46c663c1d71c02ae525dd01eabe20ca7d8e2f0bb
Batuhan KATIRCI <katircib@gmail.com>
2024-11-30 02:49:50 +0300

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [{ src: "src/public", dest: "assets" }],
+    }),
+  ],
   optimizeDeps: {
     exclude: ["lucide-react"],
   },
