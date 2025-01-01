6d66b76e713dfa73f78f2ff28c4fd15b17f7b19a
Batuhan KATIRCI <batu@atolye15.com>
2025-01-01 23:42:42 +0300

    ads txt

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,20 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { viteStaticCopy } from "vite-plugin-static-copy";
 
+import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
   plugins: [
     react(),
     viteStaticCopy({
-      targets: [{ src: "src/public", dest: "assets" }],
+      targets: [
+        { src: "src/public", dest: "assets" },
+        {
+          src: "src/public/root/*",
+          dest: path.resolve(__dirname, "./dist"),
+        },
+      ],
     }),
   ],
   optimizeDeps: {
